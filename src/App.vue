42b236e7f02519354efbcc0e2bd4f21f01ee4cc6
Shaun4727 <shaun.mononsoft@gmail.com>
2023-03-14 12:05:00 +0600

    login page designed

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <component :is="resolveComponent">
+        <router-view/>
+    </component>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import LoginView from "@/views/login/login.vue"
 
-nav {
-  padding: 30px;
+export default{
+    components:{
+        LoginView
+    },
+    computed: {
+        resolveComponent(){
+            
+            return "LoginView"
+        }
+    },
 }
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+<style scoped>
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/views/login/login.vue
@@ -0,0 +1,48 @@
+<template>
+    <div id="login-container">
+      <div id="left-column"></div>
+      <div id="branding">
+        <img src="@/assets/images/logo.png" />
+        <h1>Login - Chat Application</h1>
+      </div>
+      <div id="login-form">
+        <form method="post" action="/">
+          <input
+            type="text"
+            name="username"
+            placeholder="enter mobile or email"
+            value=""
+          />
+          <p class="error show">Error message here</p>
+          <input type="password" name="password" placeholder="enter password" />
+          <input type="submit" value="Login" />
+        </form>
+      </div>
+    </div>
+</template>
+  
+  <script>
+  // @ is an alias to /src
+//   import HelloWorld from '@/components/HelloWorld.vue'
+  
+  export default {
+    name: 'LoginView',
+    components: {
+    },
+    data(){
+        return {
+
+        }
+    },
+    computed: {
+
+    },
+    methods:{
+
+    }
+  }
+  </script>
+  
+<style scoped>
+
+</style>
